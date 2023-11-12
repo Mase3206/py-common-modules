e3561ec518368577e6d39c22b61693d15de50fc3
Noah S. Roberts <35052448+Mase3206@users.noreply.github.com>
2023-11-11 17:19:58 -0700

    update dependencies.py to support direct and indirect execution

--- dependencies.py
+++ dependencies.py
@@ -1,43 +1,27 @@
-import importlib, subprocess, argparse
+import importlib, subprocess, argparse, sys
 
-parser = argparse.ArgumentParser(description='Python pip package dependency checker and satisfier')
-parser.add_argument('os', type=str, help='Operating system family of this device (valid: macos, linux, windows)')
-parser.add_argument('packages', type=str, help='Name of single package')
-
-args = parser.parse_args()
-package = args.packages 	# str
-os = args.os				# str
+global packageName, moduleName, specialModules
 
 specialModules = {	
 	# package name : module name
 	"pyyaml": "yaml"
 }
 
-global packageName, moduleName
 
-packageName = ''
-moduleName = ''
-
-
-
-if os not in ['macos', 'linux', 'windows']:
-	raise ValueError(f'OS name "{os}" is not valid. Valid OS families are macos, linux, and windows.')
-
-
-def special():
-	global packageName, moduleName
+def special(packageName):
 	if package in list(specialModules.keys()):
-		packageName = package
-		moduleName = specialModules[package]
+		moduleName = specialModules[packageName]
 	else:
-		packageName = moduleName = package
+		moduleName = package
+
+	return moduleName
 
 
-def check():
+def check(module):
 	installed = None
 	
 	try:
-		tempImport = importlib.import_module(moduleName)
+		tempImport = importlib.import_module(module)
 		installed = True
 		del tempImport
 	except:
@@ -46,14 +30,34 @@ def check():
 	return installed
 
 
-def installRequired(opsys:str):
-	command = ['python', '-m', 'pip', 'install', packageName]
+def installRequired(os:str, package:str):
+	command = ['python', '-m', 'pip', 'install', package]
 	
-	if opsys == 'macos':
+	if os == 'macos':
 		command[0] = 'python3'
 
 	subprocess.call(command)
 
-special()
-if not check():
-	installRequired(os)
+
+# This code will only run if the program is called directly, because it all depends on command line arguments.
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Python pip package dependency checker and satisfier')
+	parser.add_argument('os', type=str, help='Operating system family of this device (valid: macos, linux, windows)')
+	parser.add_argument('packages', type=str, help='Name of single package')
+
+	args = parser.parse_args()
+	package = args.packages 	# str
+	os = args.os				# str
+
+	if os not in ['macos', 'linux', 'windows']:
+		raise ValueError(f'OS name "{os}" is not valid. Valid OS families are macos, linux, and windows.')
+	
+	module = special(package)
+	if check(module):
+		print(f'Module "{module}" from package "{package}" is already installed.')
+	else:
+		sys.stdout.flush()
+		sys.stdout.write(f'Module "{module}" from package "{package}" is not installed. Installing via pip...')
+		installRequired(os, package)
+		sys.stdout.write(f'\rModule "{module}" from package "{package}" is not installed. Installing via pip... done.')
